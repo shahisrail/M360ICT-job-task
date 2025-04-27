c8b83f0480b42e91f6c02f75915fdbc1c30b6857
shahisrail <shahisrail134@gmail.com>
2025-04-27 20:10:07 +0600

    setup store and wrap this main win store provider

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,21 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Error from "../ Error";
+import Home from "../components/Home";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement: <Error message="An unexpected error occurred"></Error>,
-      children: [
-        {
-          path: "/",
-          element: <Addschool />,
-        },
-   
-      ],
-    },
-  ]);
-
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error message="An unexpected error occurred"></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 export default router;
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      Helloe home page
+    </div>
+  );
+};
+
+export default Home;
